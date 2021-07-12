ce0b0158b4a070ae00f43e8bba766861290e70e0
JonnyMurillo288 <39134582+JonnyMurillo288@users.noreply.github.com>
2021-07-12 15:15:22 -0700

    Add files via upload

--- edgeWeightedGraphs/graphs/dijkstras.go
+++ edgeWeightedGraphs/graphs/dijkstras.go
@@ -1,7 +1,6 @@
 package graphs
 
 import (
-	"fmt"
 	"math"
 
 	utils "github.com/Jonnymurillo288/GoUtils"
@@ -29,7 +28,6 @@ func NewDijkstras(g *EdgeWeightDigraph, s int) Dijkstras {
 
 	d.PQ.Insert(s,0.0) // relax vertices in order of distance from s
 	for !d.PQ.IsEmpty() {
-		fmt.Println(d.PQ)
 		v := d.PQ.DelMin()
 		for _,e := range g.Adj[v] {
 			d.Relax(&e)
